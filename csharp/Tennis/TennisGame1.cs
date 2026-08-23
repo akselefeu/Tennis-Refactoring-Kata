6f4d3efe960576a979f54884ffd5e74fbfa65700
akselefeu <30258440+akselefeu@users.noreply.github.com>
2026-08-23 22:52:19 +0300

    Use Player model in TennisGame1
    Replace raw score fields with Player instances and delegate point increments to Player.AddPoint while keeping the scoring behavior unchanged.

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -4,38 +4,36 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private readonly Player _player1;
+        private readonly Player _player2;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            _player1 = new Player(player1Name);
+            _player2 = new Player(player2Name);
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                m_score1 += 1;
+            if (_player1.Name == playerName)
+                _player1.AddPoint();
             else
-                m_score2 += 1;
+                _player2.AddPoint();
         }
 
         public string GetScore()
         {
-            if (m_score1 == m_score2)
+            if (_player1.Score == _player2.Score)
             {
-                return GetEqualScore(m_score1);
+                return GetEqualScore(_player1.Score);
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (_player1.Score >= 4 || _player2.Score >= 4)
             {
-                return GetAdvantageOrWinScore(m_score1, m_score2);
+                return GetAdvantageOrWinScore(_player1.Score, _player2.Score);
             }
             else
             {
-                return GetScoreName(m_score1) + "-" + GetScoreName(m_score2);
+                return GetScoreName(_player1.Score) + "-" + GetScoreName(_player2.Score);
             }
         }
 
